c9d314bdd76b14cf18ce1d2d28e8eff295ff48d7
Melody Kelly <melody@melody.blue>
2018-07-06 17:22:58 +1000

    Almost done with UI integration testing.

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,59 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class BibliotecaApp {
 
+    private static Biblioteca library;
+    private static Scanner sc;
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Welcome to Biblioteca!\nPress any key to select an option.");
+        sc = new Scanner(System.in);
+        List<Book> books = new ArrayList<Book>();
+        books.add(new Book("The Conquest of Bread", "Peter Kropotkin", 1892));
+        books.add(new Book("Capital. Critique of Political Economy ", "Karl Marx", 1867));
+        books.add(new Book("The Ego and Its Own", "Max Stirner", 1845));
+        library = new Biblioteca(books);
+
+        MenuItem[] menuItems = {
+                new MenuItem("List books", BibliotecaApp::listBooks),
+                new MenuItem("Checkout book", BibliotecaApp::checkoutBook),
+                new MenuItem("Return book", BibliotecaApp::returnBook)
+        };
+
+        Menu menu = new Menu(menuItems);
+        while (!menu.isDone()) {
+            try {
+                System.out.println(menu);
+                menu.input(sc.nextLine().replace("\n", ""));
+            } catch (Menu.InvalidMenuSelectionException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static void returnBook() {
+        System.out.println("Please enter the name of the book to return: ");
+        if (library.returnBook(sc.nextLine())) {
+            System.out.println("Thank you for returning the book.\n");
+        } else {
+            System.out.println("That is not a valid book to return.\n");
+        }
+    }
+
+    private static void checkoutBook() {
+        System.out.println("Please enter the name of the book to checkout: ");
+        if (library.checkoutBook(sc.nextLine())) {
+            System.out.println("Thank you! Enjoy the book.\n");
+        } else {
+            System.out.println("That book is not available.\n");
+        }
+    }
+
+    public static void listBooks() {
+        System.out.println(library.listBookDetails());
     }
 }
--- test/com/twu/biblioteca/UserInterfaceTest.java
+++ test/com/twu/biblioteca/UserInterfaceTest.java
@@ -2,18 +2,24 @@ package com.twu.biblioteca;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class UserInterfaceTest {
     private BibliotecaApp bib;
-    private ByteArrayOutputStream output;
-    private ByteArrayInputStream input;
+    private ByteArrayOutputStream outputStream;
+    private ByteArrayInputStream inputStream;
 
     @Before
     public void setUp() {
-        output = new ByteArrayOutputStream();
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
         bib = new BibliotecaApp();
     }
 
@@ -23,12 +29,100 @@ public class UserInterfaceTest {
         System.setOut(System.out);
     }
 
+    @Test
+    public void welcomeMessageTest() {
+        final String input = "0";
+        final String output = "Welcome to Biblioteca!\nPress any key to select an option.\n";
+        setInput(input);
+        BibliotecaApp.main(new String[0]);
+        assertTrue(getOutput().contains(output));
+    }
+
+    @Test
+    public void listBooksTest() {
+        final String input = "1\n0";
+        final String output = "Welcome to Biblioteca!\n" +
+                "Press any key to select an option.\n" +
+                " 1 | List books\n" +
+                " 2 | Checkout book\n" +
+                " 3 | Return book\n" +
+                " 0 | Quit\n" +
+                "\n" +
+                "+-----------------------------------------+-----------------+------+\n" +
+                "| Title                                   | Author          | Year |\n" +
+                "+-----------------------------------------+-----------------+------+\n" +
+                "| The Conquest of Bread                   | Peter Kropotkin | 1892 |\n" +
+                "| Capital. Critique of Political Economy  | Karl Marx       | 1867 |\n" +
+                "| The Ego and Its Own                     | Max Stirner     | 1845 |\n" +
+                "+-----------------------------------------+-----------------+------+\n" +
+                "\n" +
+                " 1 | List books\n" +
+                " 2 | Checkout book\n" +
+                " 3 | Return book\n" +
+                " 0 | Quit\n\n";
+        setInput(input);
+        BibliotecaApp.main(new String[0]);
+        assertEquals(output, getOutput());
+    }
+
+    @Test
+    public void checkoutBooksTest() {
+        final String input = "2\nThe Conquest of Bread\n0";
+        final String output = "Welcome to Biblioteca!\n" +
+                "Press any key to select an option.\n" +
+                " 1 | List books\n" +
+                " 2 | Checkout book\n" +
+                " 3 | Return book\n" +
+                " 0 | Quit\n" +
+                "\n" +
+                "Please enter the name of the book to checkout: \n" +
+                "Thank you! Enjoy the book.\n" +
+                "\n" +
+                " 1 | List books\n" +
+                " 2 | Checkout book\n" +
+                " 3 | Return book\n" +
+                " 0 | Quit\n\n";
+        setInput(input);
+        BibliotecaApp.main(new String[0]);
+        assertEquals(output, getOutput());
+    }
+
+    @Test
+    public void returnBooksTest() {
+        final String input = "2\nThe Conquest of Bread\n3\nThe Conquest of Bread\n0";
+        final String output = "Welcome to Biblioteca!\n" +
+                "Press any key to select an option.\n" +
+                " 1 | List books\n" +
+                " 2 | Checkout book\n" +
+                " 3 | Return book\n" +
+                " 0 | Quit\n" +
+                "\n" +
+                "Please enter the name of the book to checkout: \n" +
+                "Thank you! Enjoy the book.\n" +
+                "\n" +
+                " 1 | List books\n" +
+                " 2 | Checkout book\n" +
+                " 3 | Return book\n" +
+                " 0 | Quit\n" +
+                "\n" +
+                "Please enter the name of the book to return: \n" +
+                "Thank you for returning the book.\n" +
+                "\n" +
+                " 1 | List books\n" +
+                " 2 | Checkout book\n" +
+                " 3 | Return book\n" +
+                " 0 | Quit\n\n";
+        setInput(input);
+        BibliotecaApp.main(new String[0]);
+        assertEquals(output, getOutput());
+    }
+
     private String getOutput() {
-        return output.toString();
+        return outputStream.toString().replace("\r", "");
     }
 
     private void setInput(String data) {
-        input = new ByteArrayInputStream(data.getBytes());
-        System.setIn(input);
+        inputStream = new ByteArrayInputStream(data.getBytes());
+        System.setIn(inputStream);
     }
 }
